a803f4ff1ec259e5e9de646f9563da6f36d08704
RusManer <essbetito@gmail.com>
2024-11-18 20:38:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,21 @@ toggleButton.addEventListener("click", () => {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle("close");
 });
+// Evento para cambiar modo con Control + M
+document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.key === 'm') {
+        event.preventDefault();
+        toggleDarkMode();
+    }
+});
 
+// Evento para redirigir a la página de pedidos con Control + P
+document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && event.key === 'p') {
+        event.preventDefault();
+        window.location.href = pedidosLink.href;
+    }
+});
 // Cambiar entre modo claro y oscuro al hacer clic en el interruptor
 const modeSwitch = document.querySelector(".toggle-switch");
 modeSwitch.addEventListener("click", () => {
@@ -99,6 +113,24 @@ modeSwitch.addEventListener("click", () => {
 });
 
 // Registrar el Service Worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js').then(() => {
+        console.log('Service Worker registrado con éxito.');
+    }).catch((error) => {
+        console.log('Error al registrar el Service Worker:', error);
+    });
+}
+// Detectar pérdida de conexión a Internet y mostrar error 404
+window.addEventListener('offline', function() {
+    // Redirigir a la página 404 cuando se pierde la conexión
+    window.location.href = '404.html';
+});
+// Detectar reconexión a Internet y redirigir a la interfaz de pedidos sin recargar
+window.addEventListener('online', function() {
+    // Redirigir a la interfaz de pedidos al reconectarse
+    window.location.href = 'index.html'; // Cambia esta ruta a la URL que deseas en caso de reconexión
+});
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js').then(() => {
         console.log('Service Worker registrado con éxito.');
